69ffa2627e51dba29a2d1ad05ccb91a373b71b7f
Aleksey Razbakov <aleksey@razbakov.com>
2022-08-23 00:59:27 +0200

    formkit

--- pages/events/[id]/edit.vue
+++ pages/events/[id]/edit.vue
@@ -1,3 +1,15 @@
 <template>
-  <div>Event Edit</div>
+  <div>
+    <h4 class="font-bold text-lg mb-4">Organise Dance Event</h4>
+    <FormKit
+      type="text"
+      label="Name"
+      help="Make it cool"
+      outer-class="mb-5"
+      label-class="block mb-1 font-bold text-sm"
+      inner-class="max-w-md border border-gray-400 rounded-lg mb-1 overflow-hidden focus-within:border-blue-500"
+      input-class="w-full h-10 px-3 border-none text-base text-gray-700 placeholder-gray-400"
+      help-class="text-xs text-gray-500"
+    />
+  </div>
 </template>
