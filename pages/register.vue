8f229dfbe496b098576d27d884bf4be584e5cbfa
Emmanuel Robinson <emmanuelorobinson@github.com>
2022-10-19 20:44:06 +0400

    vercel test image fix

--- pages/register.vue
+++ pages/register.vue
@@ -14,8 +14,7 @@
     >
       <div class="space-y-4">
         <img
-          src="
-        ./static/svg/logo-horizontal-dark.svg"
+          src=""
           alt=""
           class="w-40 justify-center align-middle mx-auto"
         />
@@ -101,6 +100,8 @@ const classes = {
   input:
     'block text-sm w-full px-3 py-2 transition duration-100 ease-in-out border rounded shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed text-black placeholder-gray-400 bg-white border-gray-300 focus:border-blue-500 ',
 }
+
+const logo = "./static/svg/logo-horizontal-dark.svg"
 
 export default {
   data() {
