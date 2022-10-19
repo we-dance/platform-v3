12d10e44c1f3b48bad7d10d1cda0bd180ffcd105
Aleksey Razbakov <aleksey@razbakov.com>
2022-10-19 19:46:10 +0200

    add i18n support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
-import { defineNuxtConfig } from 'nuxt'
-
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  ssr: false,
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss', '@intlify/nuxt3'],
+  intlify: {
+    localeDir: 'locales',
+    vueI18n: {
+      locale: 'en',
+    },
+  },
 })
