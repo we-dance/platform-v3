46f921d9734901f7f5abd4dce48e416354a09b48
Firat92 <firattezcan92@gmail.com>
2023-02-24 16:15:37 +0300

    install vue heroicons

--- components/MainButton.vue
+++ components/MainButton.vue
@@ -4,7 +4,15 @@
     <TButton type="secondary" label="secondary button"></TButton>
     <TButton type="tertiary" label="tertiary button"></TButton>
     <TButton type="danger" label="destructive button"></TButton>
+    <TButton
+      type="danger"
+      label="destructive button"
+      icon="AnnotationIcon "
+    ></TButton>
+    <AnnotationIcon size="1.5x" />
   </div>
 </template>
 
-<script></script>
+<script>
+import { AnnotationIcon } from '@vue-hero-icons/outline'
+</script>
